44eb5682a42a05aa5099eca5e31ac462cdcad846
Sarah Noles <noles.sarah@gmail.com>
2019-12-29 09:35:01 -0600

    Update reading_from_excel.py

--- Reading_Excel/reading_from_excel.py
+++ Reading_Excel/reading_from_excel.py
@@ -1,1 +1,19 @@
+import pandas as pd
 
+# General Import
+data1 = pd.read_excel('readme.xlsx', 'data')
+
+# Fix all nan values
+data2 = pd.read_excel('readme.xlsx', 'data')
+data2 = data2.fillna('')
+
+# Fix nan values by column
+data3 = pd.read_excel('readme_v2.xlsx', 'data')
+data3 = data3.fillna({'breed': '', 'puppy': False})
+
+# Fix nan values in specific columns
+data4 = pd.read_excel('readme_v2.xlsx', 'data')
+data4 = data4.fillna({'breed': ''})
+
+# Fix column data type
+data5 = pd.read_excel('readme_v2.xlsx', 'data', dtype={'puppy': bool})
